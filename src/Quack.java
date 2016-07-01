6fefb00efc3b44e203f884ee74675c2337ccab64
ak11234 <akpc556@gmail.com>
2016-07-01 13:36:25 -0500

    Initial commit

---
+++ src/Quack.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Adam Kalman on 7/1/2016. Lab 5
+ */
+public class Quack implements QuackInterfaces {
+    public static void main(String[] args){
+
+    }
+    public boolean inFront(Object element){
+
+        return false;
+    }
+    public boolean inBack(Object element){
+
+        return false;
+    }
+    public Object outFront(){
+
+        return null;
+    }
+    public Object outBack(){
+
+        return null;
+    }
+}
---
+++ src/QuackInterfaces.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Adam Kalman on 7/1/2016. Lab 5
+ */
+public interface QuackInterfaces {
+
+    public boolean inFront(Object element);
+    public boolean inBack(Object element);
+    public Object outFront();
+    public Object outBack();
+
+}
